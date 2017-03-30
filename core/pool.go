be16db17b19ecf4e4c092d7778f717fbca6e69bd
Xiaoyao <qian13@illinois.edu>
2017-03-29 19:20:52 -0500

    throughput metric (#1)
    * throughput metric
    
    * added cumulative throughput

--- core/pool.go
+++ core/pool.go
@@ -3,6 +3,7 @@ package core
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 type Pool struct {
@@ -13,23 +14,33 @@ func (p Pool) Start(engine Engine, provider RequestProvider, metricsManager Metr
 	wg.Add(config.Concurrency)
 
 	queue := provider.Provide()
+	go metricsManager.MeasureThroughput()
+	go metricsManager.SampleThroughput()
+	//START SENDING REQUEST
+	throughputQueue := metricsManager.GetQueue()
+	startTime := time.Now()
 	for i := 0; i < config.Concurrency; i++ {
 		go func(i int) {
 			defer wg.Done()
 			for req := range queue {
 				metricsManager.RecordRequest(req)
-				fmt.Println(fmt.Sprintf("goroutine %d running request...", i))
+				// fmt.Println(fmt.Sprintf("goroutine %d running request...", i))
 				resp, err := engine.RunRequest(req)
+				throughputQueue <- 1
 				if err != nil {
 					fmt.Println(err)
 					metricsManager.RecordError(err)
 					continue
 				}
 				metricsManager.RecordResponse(resp)
-				fmt.Println("\t" + resp.Status())
+				// fmt.Println("\t" + resp.Status())
 			}
 		}(i)
 	}
-
 	wg.Wait()
+	elapseTime := time.Since(startTime)
+	metricsManager.RecordThroughput(elapseTime.Seconds())
+	close(throughputQueue)
+	// metricsManager.RecordThroughput(float64(requestSent) / elapseTime.Seconds())
+
 }
--- impl/httpMetricsManager.go
+++ impl/httpMetricsManager.go
@@ -1,8 +1,16 @@
 package impl
 
-import "github.com/xiaoyao1991/chukonu/core"
+import (
+	"fmt"
+	"time"
+
+	"github.com/xiaoyao1991/chukonu/core"
+)
 
 type HttpMetricsManager struct {
+	requestSent int
+	countQueue  chan int
+	// throughputRecord []int
 }
 
 func (m *HttpMetricsManager) RecordRequest(request core.ChukonuRequest) {
@@ -10,4 +18,44 @@ func (m *HttpMetricsManager) RecordRequest(request core.ChukonuRequest) {
 func (m *HttpMetricsManager) RecordResponse(response core.ChukonuResponse) {
 }
 func (m *HttpMetricsManager) RecordError(err error) {
+}
+
+func (m *HttpMetricsManager) RecordThroughput(t float64) {
+	fmt.Printf("Sent: %v, time: %v, throughput: %v\n", m.requestSent, t, float64(m.requestSent)/t)
+}
+func (m *HttpMetricsManager) GetQueue() chan int {
+	return m.countQueue
+}
+func (m *HttpMetricsManager) GetThroughput() int {
+	return m.requestSent
+}
+
+func NewHttpMetricsManager() *HttpMetricsManager {
+	manager := new(HttpMetricsManager)
+	manager.requestSent = 0
+	manager.countQueue = make(chan int, 10)
+	// manager.throughputRecord = make([]int, 0, 3600)
+	return manager
+}
+
+func (m *HttpMetricsManager) MeasureThroughput() {
+	// startTime := time.Now()
+	for count := range m.countQueue {
+		m.requestSent = m.requestSent + count
+	}
+}
+
+func (m *HttpMetricsManager) SampleThroughput() {
+	preRequestSent := m.requestSent
+	windowRequestSent := 0
+	startTime := time.Now()
+	tick := time.Tick(1 * time.Second)
+	for {
+		<-tick
+		windowRequestSent = m.requestSent - preRequestSent
+		preRequestSent = m.requestSent
+		elapseTime := time.Since(startTime)
+		fmt.Println("Throughput/sec: ", windowRequestSent)
+		fmt.Println("cumulative: ", float64(m.requestSent)/elapseTime.Seconds())
+	}
 }
