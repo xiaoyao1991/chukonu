5a52d248d58322d181ca2695aa7d16208e162684
Xiaoyao <qian13@illinois.edu>
2017-05-07 00:59:42 -0500

    Revert "Aggregate Metric (#8)"
    This reverts commit 1fc740bbaf82a351c100862a1c4b88b275449561.

--- core/types.go
+++ core/types.go
@@ -91,12 +91,15 @@ type Engine interface {
 }
 
 type MetricsManager interface {
+	RecordRequest(request ChukonuRequest)
+	RecordResponse(response ChukonuResponse)
+	RecordError(err error)
+	// RecordThroughput(throughput float64)
+	GetQueue() chan int
 	MeasureThroughput()
 	RecordThroughput(sec float64)
 	GetThroughput() int
 	SampleMetrics()
-	StartRecording() (chan int, chan ChukonuRequest, chan ChukonuResponse, chan error)
-	SetConsulConfig(tenantId string, cid string, consulAddress string)
 }
 
 type LogReplayer interface {
--- impl/httpMetricsManager.go
+++ impl/httpMetricsManager.go
@@ -1,54 +1,52 @@
 package impl
 
 import (
-	"bytes"
-	"encoding/gob"
 	"fmt"
 	"sort"
-	"sync"
 	"time"
 
-	"github.com/hashicorp/consul/api"
 	"github.com/xiaoyao1991/chukonu/core"
 )
 
 type HttpMetricsManager struct {
-	requestSent   int
-	durationSum   time.Duration
-	numErrors     int
-	numRecords    int
-	histogram     map[time.Duration]int
-	timeFromStart time.Duration
-	tenantId      string
-	cid           string
-
-	// channels
-	countQueue    chan int
-	requestQueue  chan core.ChukonuRequest
-	responseQueue chan core.ChukonuResponse
-	errorQueue    chan error
-
-	//locks
-	sync.Mutex
-
-	//APIs
-	consul *api.Client
+	requestSent int
+	countQueue  chan int
+	records     HttpRecords
+	durationSum DurationSummation
+	numErrors   int
+	// throughputRecord []int
 }
-type Snapshot struct {
-	RequestSent int
-	DurationSum time.Duration
-	NumErrors   int
-	NumRecords  int
-	Histogram   map[time.Duration]int
+
+// type HttpRecord struct {
+// 	duration time.Duration
+// 	id       uuid.UUID
+// 	status   string
+// }
+type HttpRecords []ChukonuHttpResponse
+
+type DurationSummation struct {
+	sum   time.Duration
+	index int
 }
+
+// ------------sorting interface for struct HttpRecord-----------
+func (slice HttpRecords) Len() int {
+	return len(slice)
+}
+func (slice HttpRecords) Less(i, j int) bool {
+	return slice[i].duration < slice[j].duration
+}
+func (slice HttpRecords) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
+
+//----------------------------------------------------------------
 
 func (m *HttpMetricsManager) RecordRequest(request core.ChukonuRequest) {
 }
 func (m *HttpMetricsManager) RecordResponse(response core.ChukonuResponse) {
 	if response, ok := response.(ChukonuHttpResponse); ok {
-		m.histogram[response.Duration()/time.Millisecond] += 1
-		m.numRecords += 1
-		m.durationSum += response.Duration() / time.Millisecond
+		m.records = append(m.records, response)
 	} else {
 		panic("Response not a HTTPResponse")
 	}
@@ -56,53 +54,33 @@ func (m *HttpMetricsManager) RecordResponse(response core.ChukonuResponse) {
 }
 func (m *HttpMetricsManager) RecordError(err error) {
 	m.numErrors += 1
-	m.numRecords += 1
 }
 func (m *HttpMetricsManager) RecordThroughput(t float64) {
 	fmt.Printf("Sent: %v, time: %v, throughput: %v\n", m.requestSent, t, float64(m.requestSent)/t)
 }
-func (m *HttpMetricsManager) GetThroughputQueue() chan int {
+func (m *HttpMetricsManager) GetQueue() chan int {
 	return m.countQueue
-}
-func (m *HttpMetricsManager) GetRequestQueue() chan core.ChukonuRequest {
-	return m.requestQueue
-}
-func (m *HttpMetricsManager) GetResponseQueue() chan core.ChukonuResponse {
-	return m.responseQueue
-}
-func (m *HttpMetricsManager) GetErrorQueue() chan error {
-	return m.errorQueue
 }
 func (m *HttpMetricsManager) GetThroughput() int {
 	return m.requestSent
 }
 
 // return the sampletime(duration) at input percentile
-func (m *HttpMetricsManager) GetDurationAt(thres []int) []time.Duration {
-	percentiles := []time.Duration{}
-	keySet := []time.Duration{}
-	for key := range m.histogram {
-		keySet = append(keySet, key)
+func (m *HttpMetricsManager) GetDurationAt(thre int) time.Duration {
+	if !sort.IsSorted(m.records) {
+		sort.Sort(m.records)
 	}
-	sort.Slice(keySet, func(i, j int) bool { return keySet[i] < keySet[j] })
-	for _, thre := range thres {
-		targetCount := thre * m.numRecords / 100
-		count := 0
-		percentile := time.Duration(-1)
-		for _, key := range keySet {
-			count += m.histogram[key]
-			if count >= targetCount {
-				percentile = key
-				break
-			}
-		}
-		percentiles = append(percentiles, percentile)
-	}
-	return percentiles
+	queryIndx := len(m.records) * thre / 100
+	return m.records[queryIndx].duration / time.Millisecond
 }
-
 func (m *HttpMetricsManager) GetMeanDuration() time.Duration {
-	return time.Duration(int(m.durationSum) / m.numRecords)
+	sum := m.durationSum.sum
+	for i := m.durationSum.index + 1; i < len(m.records); i++ {
+		sum += m.records[i].duration
+	}
+	m.durationSum.index = len(m.records)
+	m.durationSum.sum = sum
+	return time.Duration(int(sum)/len(m.records)) / time.Millisecond
 }
 
 // Data for each response shoud be periodically dump to disk
@@ -110,124 +88,20 @@ func (m *HttpMetricsManager) DumpToDisk() {
 	// TODO:
 }
 
-func (m *HttpMetricsManager) SetConsulConfig(tenantId string, cid string, consulAddress string) {
-	m.tenantId = tenantId
-	m.cid = cid
-	consul, err := api.NewClient(&api.Config{Address: consulAddress})
-	if err != nil {
-		panic(err)
-	} else {
-		m.consul = consul
-	}
-}
-
 func NewHttpMetricsManager() *HttpMetricsManager {
 	manager := HttpMetricsManager{}
 	manager.requestSent = 0
-	manager.durationSum = 0
-	manager.histogram = make(map[time.Duration]int)
-	manager.numErrors = 0
-	manager.numRecords = 0
-	manager.timeFromStart = time.Duration(0)
-
 	manager.countQueue = make(chan int, 10)
-	manager.requestQueue = make(chan core.ChukonuRequest, 10)
-	manager.responseQueue = make(chan core.ChukonuResponse, 10)
-	manager.errorQueue = make(chan error, 10)
-
-	/*FOR DEV ONLY, MUST REMOVE LATER*/
-	var consulAddress = "http://192.168.1.10:8500"
-	manager.tenantId = "druidtest"
-	manager.cid = "test"
-	consul, err := api.NewClient(&api.Config{Address: consulAddress})
-	if err != nil {
-		panic(err)
-	} else {
-		manager.consul = consul
-	}
-	/*FOR DEV ONLY, MUST REMOVE LATER*/
+	manager.records = []ChukonuHttpResponse{}
+	manager.durationSum = DurationSummation{}
+	manager.numErrors = 0
+	// manager.throughputRecord = make([]int, 0, 3600)
 	return &manager
 }
 
 func (m *HttpMetricsManager) MeasureThroughput() {
 	for count := range m.countQueue {
-		m.Lock()
 		m.requestSent = m.requestSent + count
-		m.Unlock()
-	}
-}
-
-func (m *HttpMetricsManager) StartRecording() (chan int, chan core.ChukonuRequest, chan core.ChukonuResponse, chan error) {
-	fmt.Printf("cid: %s\n", m.cid)
-	go m.MeasureThroughput()
-	go m.SampleMetrics()
-	go func() {
-		for request := range m.requestQueue {
-			m.Lock()
-			m.RecordRequest(request)
-			m.Unlock()
-		}
-	}()
-	go func() {
-		for response := range m.responseQueue {
-			m.Lock()
-			m.RecordResponse(response)
-			m.Unlock()
-		}
-	}()
-	go func() {
-		for error := range m.errorQueue {
-			m.Lock()
-			m.RecordError(error)
-			m.Unlock()
-		}
-	}()
-	return m.GetThroughputQueue(), m.GetRequestQueue(), m.GetResponseQueue(), m.GetErrorQueue()
-}
-
-func (m *HttpMetricsManager) UploadSnapshot() {
-	snapshot := Snapshot{
-		RequestSent: m.requestSent,
-		DurationSum: m.durationSum,
-		NumErrors:   m.numErrors,
-		NumRecords:  m.numRecords,
-		Histogram:   m.histogram,
-	}
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	err := enc.Encode(snapshot)
-	if err != nil {
-		panic(err)
-	}
-	kv := m.consul.KV()
-	kvpair := &api.KVPair{
-		Key:   fmt.Sprintf("%s/snapshot/%s/%d", m.tenantId, m.cid, int(m.timeFromStart.Seconds())),
-		Value: buf.Bytes(),
-	}
-	_, err = kv.Put(kvpair, nil)
-	if err != nil {
-		// TODO:
-		fmt.Print("save snapshot error: ")
-		fmt.Println(err)
-	}
-}
-
-func (m *HttpMetricsManager) DownloadSnapshot() {
-	kv := m.consul.KV()
-	result, _, err := kv.Get(fmt.Sprintf("%s/snapshot/%s/%s", m.tenantId, m.cid, m.timeFromStart), nil)
-	if err != nil {
-		panic(err)
-	}
-	buf := bytes.NewBuffer(result.Value)
-	dec := gob.NewDecoder(buf)
-	snaphotOut := Snapshot{}
-	err = dec.Decode(&snaphotOut)
-	if err != nil {
-		fmt.Println("decode error:", err)
-	} else {
-		for k, v := range snaphotOut.Histogram {
-			fmt.Printf("key: %v, value: %v", k, v)
-		}
 	}
 }
 
@@ -237,48 +111,26 @@ func (m *HttpMetricsManager) SampleMetrics() {
 	deltaStartTime := batchStartTime
 	deltaTick := time.Tick(1 * time.Second)
 	batchTick := time.Tick(10 * time.Second)
-	snapshotTick := time.Tick(5 * time.Second)
 	prevRequestCount := 0
 	for {
 		select {
 		case <-deltaTick:
-			m.Lock()
 			elapsed := time.Since(deltaStartTime)
 			deltaRequestCount := m.requestSent - prevRequestCount
-			percentiles := m.GetDurationAt([]int{50, 90, 95, 99})
-			if m.numRecords == 0 {
-				m.numRecords = -1
-			}
-			fmt.Printf("delta: %f, total: %d in %f sec, err rate %f, mean %d, median %d, 90percentile %d, 95percentile %d, 99percentile %d\n", float64(deltaRequestCount)/elapsed.Seconds(), deltaRequestCount, elapsed.Seconds(), float64(m.numErrors)/float64(m.numRecords), m.GetMeanDuration(), percentiles[0], percentiles[1], percentiles[2], percentiles[3])
+			fmt.Printf("delta: %f, total: %d in %f sec, err rate %f, mean %d, median %d, 90percentile %d, 95percentile %d, 99percentile %d\n", float64(deltaRequestCount)/elapsed.Seconds(), deltaRequestCount, elapsed.Seconds(), float64(m.numErrors)/float64(len(m.records)), m.GetMeanDuration(), m.GetDurationAt(50), m.GetDurationAt(90), m.GetDurationAt(95), m.GetDurationAt(99))
 			deltaStartTime = time.Now()
 			prevRequestCount = m.requestSent
-			m.Unlock()
 		case <-batchTick:
-			m.Lock()
 			elapsed := time.Since(batchStartTime)
 			fmt.Printf("overall: %f, total: %d in %f sec\n", float64(m.requestSent)/elapsed.Seconds(), m.requestSent, elapsed.Seconds())
 
-			// test printing all response
+			// //test printing all response
 			// if m.requestSent == 100 {
-			// 	sum1 := 0
-			// 	sum2 := 0
-			// 	num := 0
-			// 	for key := range m.histogram {
-			// 		fmt.Printf("key: %d, val: %v\n", key, m.histogram[key])
-			// 		sum2 += int(key) * m.histogram[key]
-			// 		num += m.histogram[key]
-			//
+			// 	for i := 0; i < 100; i++ {
+			// 		fmt.Printf("sample time: %d, status: %v\n", m.records[i].Duration()/time.Millisecond, m.records[i].Status())
 			// 	}
-			// 	fmt.Printf("sum1: %v, sum2: %v, #: %v \n", sum1, sum2, num)
 			// 	return
 			// }
-			m.Unlock()
-		case <-snapshotTick:
-			m.timeFromStart = time.Since(batchStartTime)
-			m.Lock()
-			m.UploadSnapshot()
-			m.Unlock()
-			// m.DownloadSnapshot()
 		default:
 		}
 
